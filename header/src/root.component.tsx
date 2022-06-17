7a4296263cae858a3222cac37e9084bdee3cc5b7
JoseER211 <88160386+JoseER211@users.noreply.github.com>
2022-06-16 23:49:22 -0500

    Se sube parte de react. Pendiente de funcionamiento completo

--- header/src/root.component.tsx
+++ header/src/root.component.tsx
@@ -1,3 +1,35 @@
-export default function Root(props) {
-  return <button>The React Button</button>;
-}
+
+import React, { useEffect } from "react";
+import axios from "axios";
+import md5 from "md5";
+
+// export default function Root(props) {
+//   // return <button>The React Button</button>;
+//   return <div>The React Button</div>;
+// }
+
+const baseUrl = "http://gateway.marvel.com/v1/public/characters";
+const publicKey = "ec1a15cd526a91d47375a66e734bd307";
+const privateKey = "996210e67854f8794836e95882798116f892622e";
+
+const time = Number(new Date());
+
+const hash = md5(time + privateKey + privateKey);
+
+
+const Root: React.FC = () => {
+
+  useEffect(() => {
+    axios
+    .get(`http://gateway.marvel.com/v1/public/characters?ts=${time}&apikey${publicKey}&hash=${hash}`,
+
+    )
+    .then(response => console.log(time, response.data.data))
+    .catch(error => console.log(error));
+  }, []);
+  
+  return <div>The React Button</div>;
+};
+
+export default Root;
+
