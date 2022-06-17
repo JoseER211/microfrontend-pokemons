825bdd85f46bd97b6bb7d9f50f533b7be171ef1a
JoseER211 <88160386+JoseER211@users.noreply.github.com>
2022-06-17 15:01:43 -0500

    Se finaliza la parte de React. Tanto Angular como React funcionan.

--- header/src/root.component.tsx
+++ header/src/root.component.tsx
@@ -1,57 +1,41 @@
 
-import React, { useEffect, useState } from "react";
-import axios from "axios";
-import md5 from "md5";
-
-const baseURL = "http://gateway.marvel.com/v1/public/characters?";
-const publicKey = "ec1a15cd526a91d47375a66e734bd307";
-const privateKey = "996210e67854f8794836e95882798116f892622e";
-
-const time = Number(new Date());
-
-const hash = md5(time + privateKey + publicKey);
-
-
-
-
+import React, {useState } from "react";
 
 const Root: React.FC = () => {
 
-  const completeURL = `${baseURL}ts=${time}&apikey=${publicKey}&hash=${hash}`
+  const completeURL = "https://digimon-api.vercel.app/api/digimon"
   const [data, setData] = useState([]);
 
   const fetchAPI = async () => {
     const response = await fetch(completeURL);
     const responseJSON = await response.json();
-    setData(responseJSON.data.results)
+    setData(responseJSON)
   }
 
-  useEffect(() => {
-    fetchAPI();
-  }, [])
-
-  // useEffect(() => {
-  //   axios
-  //   .get(`${baseURL}ts=${time}&apikey=${publicKey}&hash=${hash}`,
-
-  //   )
-  //   .then(response => console.log(response.data.data.results))
-  //   .catch(error => console.log(error));
-  // }, []);
-  
   return (
     <div>
-      Hola mundo
-      <ul>
-      {
-        !data ? <h1>Cargando...</h1> :
+      <button id="b" className="btn btn-outline-primary" onClick={fetchAPI} >Descubre personajes desde React</button>
+          {
         data.map((character, index) => {
-          return <li key={index}>{character.name}</li>
+          return (
+            <div
+            className="card mb-3"
+          >
+            <div className="row no-gutters">
+              <div className="col-md-2">
+              </div>
+              <div className="col-md-8">
+                <div className="card-body">
+                  <h5 className="card-title">Name: { character.name }</h5>
+                  <p className="card-text">Level: { character.level }</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          )
         })
       }
-      </ul>
-     
-
+      
     </div>
   );
 }
