4593d4d0a7d385516f5db758f197eab3fd543510
JoseER211 <88160386+JoseER211@users.noreply.github.com>
2022-06-17 10:23:32 -0500

    Se avanza en React. Ya sí muestra el consumo de api por pantalla

--- header/src/root.component.tsx
+++ header/src/root.component.tsx
@@ -1,35 +1,60 @@
 
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 import md5 from "md5";
 
-// export default function Root(props) {
-//   // return <button>The React Button</button>;
-//   return <div>The React Button</div>;
-// }
-
-const baseUrl = "http://gateway.marvel.com/v1/public/characters";
+const baseURL = "http://gateway.marvel.com/v1/public/characters?";
 const publicKey = "ec1a15cd526a91d47375a66e734bd307";
 const privateKey = "996210e67854f8794836e95882798116f892622e";
 
 const time = Number(new Date());
 
-const hash = md5(time + privateKey + privateKey);
+const hash = md5(time + privateKey + publicKey);
+
+
+
 
 
 const Root: React.FC = () => {
 
-  useEffect(() => {
-    axios
-    .get(`http://gateway.marvel.com/v1/public/characters?ts=${time}&apikey${publicKey}&hash=${hash}`,
+  const completeURL = `${baseURL}ts=${time}&apikey=${publicKey}&hash=${hash}`
+  const [data, setData] = useState([]);
 
-    )
-    .then(response => console.log(time, response.data.data))
-    .catch(error => console.log(error));
-  }, []);
+  const fetchAPI = async () => {
+    const response = await fetch(completeURL);
+    const responseJSON = await response.json();
+    setData(responseJSON.data.results)
+  }
+
+  useEffect(() => {
+    fetchAPI();
+  }, [])
+
+  // useEffect(() => {
+  //   axios
+  //   .get(`${baseURL}ts=${time}&apikey=${publicKey}&hash=${hash}`,
+
+  //   )
+  //   .then(response => console.log(response.data.data.results))
+  //   .catch(error => console.log(error));
+  // }, []);
   
-  return <div>The React Button</div>;
-};
+  return (
+    <div>
+      Hola mundo
+      <ul>
+      {
+        !data ? <h1>Cargando...</h1> :
+        data.map((character, index) => {
+          return <li key={index}>{character.name}</li>
+        })
+      }
+      </ul>
+     
+
+    </div>
+  );
+}
 
 export default Root;
 
